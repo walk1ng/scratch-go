ae9b744c1cdce4d194a711cfa57de10451de70b7
walk1ng <iliwgg@gmail.com>
2021-08-11 23:53:58 +0800

    demo k8s update unstructured object with patch

--- k8s-update-api-object-patch/main.go
+++ k8s-update-api-object-patch/main.go
@@ -8,8 +8,10 @@ import (
 	"path"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -35,6 +37,11 @@ func main() {
 	}
 
 	clients, err := kubernetes.NewForConfig(c)
+	if err != nil {
+		panic(err)
+	}
+
+	dc, err := dynamic.NewForConfig(c)
 	if err != nil {
 		panic(err)
 	}
@@ -76,6 +83,42 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("failed to patch deployment %q: %v\n", name, err)
+		return
+	}
+
+	// unstructured
+	r := schema.GroupVersionResource{
+		Group:    "apps",
+		Version:  "v1",
+		Resource: "deployments",
+	}
+	obj, err := dc.Resource(r).Namespace("default").Get(context.Background(), name, v1.GetOptions{})
+	if err != nil {
+		fmt.Printf("failed to get unstructured: %v\n", err)
+		return
+	}
+
+	fmt.Println(obj.GetName())
+
+	// patch
+	patch, err = ioutil.ReadFile("./application/patch-file-tolerations.yaml")
+	if err != nil {
+		fmt.Printf("failed to read patch file: %v\n", err)
+		return
+	}
+
+	patch, err = yaml.ToJSON(patch)
+	if err != nil {
+		fmt.Printf("failed to convert patch yaml to patch json: %v\n", err)
+		return
+	}
+
+	_, err = dc.Resource(r).Namespace("default").Patch(context.Background(), name,
+		types.StrategicMergePatchType,
+		patch,
+		v1.PatchOptions{})
+	if err != nil {
+		fmt.Printf("failed to patch unstructured: %v\n", err)
 		return
 	}
 }
