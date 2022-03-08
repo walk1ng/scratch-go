eca0864ac885e84b96774e5b9c03dae242ca9355
walk1ng <iliwgg@gmail.com>
2022-03-08 16:29:59 +0800

    adjust some modules for older go and k8s environment, demostrate controller-runtime client works for custom resources

--- programming-k8s/clients/using-controller-runtime-client/go.mod
+++ programming-k8s/clients/using-controller-runtime-client/go.mod
@@ -1,10 +1,18 @@
 module using-controller-runtime-client
 
-go 1.16
+go 1.15
 
 require (
-	k8s.io/api v0.22.5
-	k8s.io/apimachinery v0.22.5
-	k8s.io/client-go v0.22.5
+	github.com/kudobuilder/kudo v0.19.0
+	github.com/spotahome/redis-operator v1.1.1
+	k8s.io/api v0.22.2
+	k8s.io/apimachinery v0.22.2
+	k8s.io/client-go v0.22.2
 	sigs.k8s.io/controller-runtime v0.7.2
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.19.2
+	k8s.io/apimachinery => k8s.io/apimachinery v0.19.2
+	k8s.io/client-go => k8s.io/client-go v0.19.2 // Required by client-go
 )
--- programming-k8s/clients/using-controller-runtime-client/main.go
+++ programming-k8s/clients/using-controller-runtime-client/main.go
@@ -6,11 +6,23 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	kudov1beta1 "github.com/kudobuilder/kudo/pkg/apis/kudo/v1beta1"
+	redisv1 "github.com/spotahome/redis-operator/api/redisfailover/v1"
+)
+
+const (
+	deploymentName   = "kube-ops-view"
+	defaultNamespace = corev1.NamespaceDefault
+	podName          = "myapp"
+	containerName    = "myapp"
 )
 
 func main() {
@@ -29,7 +41,7 @@ func main() {
 
 	// list pods
 	podList := &corev1.PodList{}
-	err = client.List(context.Background(), podList, runtimeclient.InNamespace("default"))
+	err = client.List(context.Background(), podList, runtimeclient.InNamespace(defaultNamespace))
 	if err != nil {
 		panic(err)
 	}
@@ -42,8 +54,8 @@ func main() {
 	// get deployment
 	deploy := appsv1.Deployment{}
 	err = client.Get(context.Background(), types.NamespacedName{
-		Namespace: "default",
-		Name:      "loki-kube-state-metrics",
+		Namespace: defaultNamespace,
+		Name:      deploymentName,
 	}, &deploy)
 	if err != nil {
 		panic(err)
@@ -53,23 +65,77 @@ func main() {
 	println("-------------------------")
 
 	// create pod
-	pod := corev1.Pod{
-		ObjectMeta: v1.ObjectMeta{
-			Namespace: "default",
-			Name:      "myapp",
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:            "myapp",
-					Image:           "nginx",
-					ImagePullPolicy: corev1.PullIfNotPresent,
-				},
-			},
-		},
+	err = client.Get(context.Background(), types.NamespacedName{Namespace: defaultNamespace, Name: podName}, &corev1.Pod{})
+	if err == nil {
+		println("skip to creating pod")
 	}
-	err = client.Create(context.Background(), &pod, &runtimeclient.CreateOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			println("pod is not existed, creating")
+			pod := corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: defaultNamespace,
+					Name:      podName,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            containerName,
+							Image:           "nginx",
+							ImagePullPolicy: corev1.PullIfNotPresent,
+						},
+					},
+				},
+			}
+			err = client.Create(context.Background(), &pod, &runtimeclient.CreateOptions{})
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Printf("failed to get pod %s/%s\n", podName, defaultNamespace)
+		}
+	}
+
+	println("-------------------------")
+
+	// custom resources
+	crScheme := runtime.NewScheme()
+	// scheme to redisfailover
+	redisv1.AddToScheme(crScheme)
+	// scheme to kudo
+	kudov1beta1.AddToScheme(crScheme)
+
+	cl, err := runtimeclient.New(config, runtimeclient.Options{
+		Scheme: crScheme,
+	})
 	if err != nil {
 		panic(err)
 	}
+
+	redisList := redisv1.RedisFailoverList{}
+	err = cl.List(context.Background(), &redisList, runtimeclient.InNamespace(defaultNamespace))
+	if err != nil {
+		panic(err)
+	}
+	println(len(redisList.Items), "redises")
+	for _, redis := range redisList.Items {
+		println(redis.Name, "/", redis.Namespace)
+	}
+
+	instanceList := kudov1beta1.InstanceList{}
+	err = cl.List(context.Background(), &instanceList)
+	if err != nil {
+		panic(err)
+	}
+	println(len(instanceList.Items), "kudo instances")
+	for _, instance := range instanceList.Items {
+		println(instance.Name, "/", instance.Namespace)
+	}
+
+	println("-------------------------")
+	/*
+		use controller-runtime client to create resource
+		which CR type implement client.Object interface
+	*/
+
 }
